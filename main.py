555f544249e4e79518f0c3abf186fa73ea06c979
esther <solidadeesther@gmail.com>
2026-06-08 13:41:48 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Started')
+pygame.init() # inicializando
+window = pygame.display.set_mode(size=(600, 480)) # criando uma janela para o jogo
+print('Setup Ended')
+
+print('Loop Started')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() #end pygame
