fb5490fd7bf58afdcc3322fa73a261caf37c1c41
esther <solidadeesther@gmail.com>
2026-06-16 19:18:31 -0300

    Add Const.py

---
+++ code/Const.py
@@ -0,0 +1,13 @@
+# c
+# COLOCAR A CONSTANTE DE COR AQUI
+
+# m
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# w
+WIN_WIDTH = 600
+WIN_HEIGHT = 480
--- code/Game.py
+++ code/Game.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame # importa a biblioteca pygame para criar jogos
-from code.menu import Menu # importa a classe Menu do arquivo menu.py dentro da pasta code
+
+from code.Menu import Menu # importa a classe Menu do arquivo Menu.py dentro da pasta code
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 
 class Game:
     def __init__(self):
         pygame.init()  # inicializando o pygame
-        self.window = pygame.display.set_mode(size=(600, 480))  # cria uma janela e guarda na variável self.window
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))  # cria uma janela e guarda na variável self.window
 
     def run(self):
         while True:
